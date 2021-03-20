1a94cb0aab27479f1f577b467aeca2b272f724a3
mirerjr <mirer.junior02@academico.ifs.edu.br>
2021-03-20 17:03:20 -0300

    Aula #2 - JPA + CRUD cliente
    + Migrations (Create table and data ADD)
    + CRUD for Clientes with data validation

---
+++ src/main/resources/db/migration/V20210319.2__adiciona-dados-iniciais-cliente.sql
@@ -0,0 +1,10 @@
+INSERT INTO cliente(nome, email, telefone)
+	VALUES ('Mirer Balbino de Andrade Junior', 'mirer.rmj@gmail.com', '99999999999'),
+	       ('Carlos Afonso de Albuquerque', 'carlos.afonso@gmail.com', '98989898989'),
+	       ('Raphael Vinhas Bilhalva', 'raphael.bilhalva@gmail.com', '98979878979'),
+	       ('Amora Taveira Estrela', 'amora.estrela@outlook.com', '98989890089'),
+	       ('Surya Freiria Baldaia', 'surya.baldaia@homail.com', '98989891119'),
+	       ('Jan Vilante Castilhos', 'janvilante.castilhos@hotmail.com', '00009898989'),
+	       ('Catalin Quinteiro Valido', 'catalin.valido@gmail.com', '421248989893'),
+	       ('Richard Barros Cangueiro', 'richard.cangueiro@gmail.com', '77777777777')
+	;
